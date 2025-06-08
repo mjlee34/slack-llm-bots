fcbac620f86f3c374c500d2ea6d337a6c061d924
mjlee34 <lee.myoung.joon@gmail.com>
2025-06-08 14:59:38 +0900

    chore: 더미 Flask 서버 debug=True로 실행

--- cheer_bot.py
+++ cheer_bot.py
@@ -106,7 +106,7 @@ def run_dummy_server():
     def index():
         return "OK"
 
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=10000, debug=True)
 
 if __name__ == "__main__":
     threading.Thread(target=run_dummy_server, daemon=True).start()
